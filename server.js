00bbdb81e9db210bd619011b640b3ecb13ecc15b
Sahil Kesarwani <80140802+SahilKesarwani@users.noreply.github.com>
2021-10-11 18:15:06 +0530

    Ready for production in Heroku

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ app.get("/updates", async (req, res) => {
 	res.json(data);
 });
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
 app.listen(PORT, () => {
 	console.log(`Server is running at port ${PORT}`);
 });
